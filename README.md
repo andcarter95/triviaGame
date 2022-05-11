90cf3df6446764498d1452ad95ca1ba01d5593a7
andcarter95 <and.carter95@gmail.com>
2022-05-10 21:29:23 -0500

    html todo done, css todo done, js todo WIP

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # triviaGame
 milestone project 1
+Trivia based game with three rounds consisting of sixteen questions per round.
+There are four catagories (Sports, History, Muisc, Geography).
+To proceed to the next round you must correctly answer 75% of the questions from the previous round.
+The game tracks your your past success per user, per round, per catagory and globally.
---
+++ apps.js
@@ -0,0 +1,25 @@
+// Create functionality to randomly generate the questions in the right round in the right order
+    // create smooth transition to the next question
+    // Create gate that doesn't allow users to next round unless they meet the qualifications    
+
+// Create button functionality that when the right answer is selected it appears green and that result is tracked
+// Create button functionality that when the wrong answer is selected it appears red and that result is tracked
+
+
+
+
+// Create a place where the data from past answers is stored for all users per catagory, per round, and per user
+
+// Create catagory data section
+    // Create sports catagory tracking
+    // Create history catagory tracking
+    // Create music catagory tracking
+    // Create geography catagory tracking
+
+// Create round and global tracking
+    // Create round 1 data tracking
+    // Create round 2 data tracking
+    // Create round 3 data tracking
+    // Create dailt streak count
+    // Create global percentile statistics
+
